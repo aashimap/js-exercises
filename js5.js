690e7a7b965f22a1a8083600fcd380f18b14f6ab
aashimap <aashima@bititude.com>
2023-07-17 18:18:56 +0530

    edit

---
+++ js5.js
@@ -0,0 +1,7 @@
+let f = prompt("Enter the temperature in Fahrenheit");
+c = ((f - 32) / 9) * 5;
+console.log("Temperature in celcius is:", c);
+
+let cel = prompt("Enter the temperature in Celcius");
+far = (c / 5) * 9 + 32;
+console.log("Temperature in Fahrenheit is:", far);
